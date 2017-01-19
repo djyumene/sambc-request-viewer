ae053d7480044aba2555dd7ebdaf9cd435086100
Yumene/Erika Mizune <yumene@gamingradio.net>
2017-01-19 10:35:56 -0800

    Update db-config.php

--- db-config.php
+++ db-config.php
@@ -3,6 +3,6 @@ mysql_connect('localhost','username','password');
 
 mysql_select_db('database');
 
-$sql="SELECT requestlist.*,songlist.artist,songlist.title,songlist.duration FROM requestlist LEFT JOIN songlist ON requestlist.songid=songlist.id";
+$sql="SELECT requestlist.*,songlist.artist,songlist.title,songlist.duration FROM requestlist LEFT JOIN songlist ON requestlist.songid=songlist.id ORDER BY t_stamp DESC LIMIT 15";
 $list=mysql_query($sql);
 ?>
